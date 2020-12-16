7d66eefdcac9176d81f26e4a8f5071ae7df9ef94
haosong <857443874@qq.com>
2020-12-16 19:24:59 +0800

    “加入创建数据库和表的各项配置”

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * @Auther: mada
+ * @Date: 2020/12/16 19:14
+ * @Description:省对象
+ */
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
